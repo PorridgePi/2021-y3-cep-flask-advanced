36bb12d8391ad47ae83af9be57a023deb5378176
PorridgePi <kunbao2006@126.com>
2021-07-11 00:05:22 +0800

    feat: Add potato.html with CSS

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from app import app
+from flask import render_template
 
 # routes are defined here
 
 @app.route('/')
 def root():
     return "Hello, World!"
+
+@app.route('/potato')
+def potato():
+    return render_template("potato.html")

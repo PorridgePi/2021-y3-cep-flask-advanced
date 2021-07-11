d34a508afb4e2414fa8c8ce09ad3a4064c9f3c42
PorridgePi <kunbao2006@126.com>
2021-07-11 09:36:21 +0800

    feat: Add validation with multiple errors on a single field

--- app/form.py
+++ app/form.py
@@ -5,12 +5,31 @@ from wtforms.fields.html5 import DateField
 from wtforms.validators import InputRequired, ValidationError
 
 from datetime import datetime
+import string
 
 class TaskForm(FlaskForm):
     name = StringField("Task Name", validators=[InputRequired()])
     description = TextAreaField("Task Description")
     tdate = DateField("Date")
     completed = BooleanField("completed?")
+
+    def validate_name(form, field):
+        # Returning more than one error when doing multiple validations on a single field
+        # It is illegal to:
+        # 1. Input a task name less than 3 characters
+        # 2. Input a task name containing punctuations
+
+        validated = True # default state of validation 
+
+        if len(field.data) < 3:
+            form.name.errors.append("Length of Task Name should be more than 3 characters.")
+            validated = False
+
+        if len([char for char in field.data if char in string.punctuation]) > 0: 
+            form.name.errors.append("Task name should not contain punctuations.")
+            validated = False
+
+        return validated
 
     def validate_tdate(form, field):
         today = datetime.today().date()

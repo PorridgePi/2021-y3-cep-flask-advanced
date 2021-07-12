29c03257f6c0138151cafd2f9c9a808c5afe4b04
PorridgePi <kunbao2006@126.com>
2021-07-12 21:58:00 +0800

    feat: Add password hashing and functions to set and check password

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from enum import unique
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     # To use a different name in the table, provide an optional first argument which is a string.
@@ -9,6 +10,15 @@ class User(db.Model):
     password = db.Column(db.String(256))
     email = db.Column(db.String(120), index=True, unique=True)
     date_created = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return "<User {}>".formate(self.username)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 def insert_dummy_data(db):
   admin = User(username="admin", email="admin@example.com", password="secretpassword")
